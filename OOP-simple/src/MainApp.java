c2cce766208d00aed7922008b9f99ed8fd4faceb
novakvova <novakvova@gmail.com>
2020-01-19 10:12:27 +0200

    Simple config one bean

--- OOP-simple/src/MainApp.java
+++ OOP-simple/src/MainApp.java
@@ -1,12 +1,19 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context=
+				new ClassPathXmlApplicationContext("ConfigDI.xml");
+		
 		//System.out.println("Hello");
-		Engine engine = new EngineElectro(); //new EngineLanos(); //new EngineAudi();
+		Engine engine =  context.getBean("myEngine", Engine.class);  //new EngineElectro(); new EngineLanos(); //new EngineAudi();
 		//engine.start();
 		Car car = new CarTesla(engine);
 		car.drive();
+		
+		context.close();
 
 	}
 
